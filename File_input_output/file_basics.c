ca35cd3cdffdd39c9430c8304c33086fdbba9ed9
Tejas Kedare <66932803+TejasKedare@users.noreply.github.com>
2020-11-25 11:52:19 +0530

    File_input_output

---
+++ File_input_output/file_basics.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *ptr;
+    //  ptr = fopen("sample.txt", "r"); // for reading a file
+    ptr = fopen("sample2.txt", "w"); // for writing in a file
+
+    return 0;
+}
---
+++ File_input_output/file_writing.c
@@ -0,0 +1,13 @@
+#include <stdio.h >
+
+int main()
+{
+    FILE *fptr;
+    int number = 45;
+    fptr = fopen("generated.txt", "w");
+    fprintf(fptr, "The number is %d \n", number);
+    fprintf(fptr, "Thanks for using fprintf %d \n", number);
+    fclose(fptr);
+
+    return 0;
+}
